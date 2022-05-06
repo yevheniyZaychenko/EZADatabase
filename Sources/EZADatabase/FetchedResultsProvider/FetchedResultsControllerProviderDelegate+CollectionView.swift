e55045471eaf824d9bd429f37403d8ab627ff1e1
Eugeniy Zaychenko <ezaychenko@reinvently.com>
2022-05-06 12:45:08 +0300

    - update only operations now only reload collectionView without animations

--- Sources/EZADatabase/FetchedResultsProvider/FetchedResultsControllerProviderDelegate+CollectionView.swift
+++ Sources/EZADatabase/FetchedResultsProvider/FetchedResultsControllerProviderDelegate+CollectionView.swift
@@ -10,10 +10,19 @@ import UIKit
 
 //MARK: - FetchedResultsProviderDelegate + CollectionView
 
+public struct ProviderOperation {
+    
+    enum OperationType {
+        case insert, delete, update, move
+    }
+    let operation: BlockOperation
+    let type: OperationType
+}
+
 public protocol CollectionViewFetchedResultsProviderDelegate: FetchedResultsProviderDelegate {
     
     var collectionView: UICollectionView! { get }
-    var operations: [BlockOperation] { get set }
+    var operations: [ProviderOperation] { get set }
     var shouldAlwaysReloadData: Bool { get }
     func didFinishAnimation()
 }
@@ -48,65 +57,70 @@ public extension CollectionViewFetchedResultsProviderDelegate {
     func moveObject(from indexPath: IndexPath?, to newIndexPath: IndexPath?) {
         
         guard let from = indexPath, let to = newIndexPath else { return }
-        addToOperations(operation: BlockOperation { [weak self] in
+        addToOperations(operation: ProviderOperation(operation: BlockOperation { [weak self] in
             self?.collectionView.deleteItems(at: [from])
             self?.collectionView.insertItems(at: [to])
-        })
+        }, type: .move))
     }
     
     func insertObject(at indexPath: IndexPath?) {
         
         let indexPaths = [indexPath].compactMap{ $0 }
-        addToOperations(operation: BlockOperation { [weak self] in
+        addToOperations(operation: ProviderOperation(operation: BlockOperation { [weak self] in
             self?.collectionView.insertItems(at: indexPaths)
-        })
+        }, type: .insert)
     }
     
     func deleteObject(at indexPath: IndexPath?) {
         
         let indexPaths = [indexPath].compactMap{ $0 }
-        addToOperations(operation: BlockOperation { [weak self] in
+            addToOperations(operation: ProviderOperation(operation: BlockOperation { [weak self] in
             self?.collectionView.deleteItems(at: indexPaths)
-        })
+            }, type: .delete)
     }
     
     func updateObject(at indexPath: IndexPath?) {
         
         let indexPaths = [indexPath].compactMap{ $0 }
         if indexPaths.isEmpty { return }
-        addToOperations(operation: BlockOperation { [weak self] in
+                addToOperations(operation: ProviderOperation(operation: BlockOperation { [weak self] in
             self?.collectionView.reloadItems(at: indexPaths)
-        })
+                }, type: .update)
     }
     
     func insert(section: Int) {
-        addToOperations(operation: BlockOperation { [weak self] in
+            addToOperations(operation: ProviderOperation(operation: BlockOperation { [weak self] in
             self?.collectionView.insertSections(IndexSet(integer: section))
-        })
+            }, type: .insert)
     }
     
     func delete(section: Int) {
-        addToOperations(operation: BlockOperation { [weak self] in
+                addToOperations(operation: ProviderOperation(operation: BlockOperation { [weak self] in
             self?.collectionView.deleteSections(IndexSet(integer: section))
-        })
+                }, type: .delete)
     }
     
     func update(section: Int) {
-        addToOperations(operation: BlockOperation { [weak self] in
+                    addToOperations(operation: ProviderOperation(operation: BlockOperation { [weak self] in
             self?.collectionView.reloadSections(IndexSet(integer: section))
-        })
+                    }, type: .update)
     }
     
-    private func addToOperations(operation: BlockOperation) {
+    private func addToOperations(operation: ProviderOperation) {
         operations.append(operation)
     }
     
     private func performBatchesOperations() {
         
         if operations.isEmpty { return }
-
+        let filtered = operations.filter { $0.type == .update }
+                        
+        if filtered.count == operations.count {
+            didFinishAnimation()
+            return
+        }
         self.collectionView.performBatchUpdates({[weak self] () -> Void in
-            self?.operations.forEach { $0.start() }
+            self?.operations.forEach { $0.operation.start() }
         }, completion: {[weak self] (finished) -> Void in
             self?.operations.removeAll()
             DispatchQueue.main.async {
